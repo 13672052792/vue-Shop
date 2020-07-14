60f1fa8fd04b578fae9b9e1a4fc273d81bfc3c72
wangyuan <themomentyuan@qq.com>
2020-07-14 18:27:27 +0800

    recommend模块

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,19 +2,22 @@
   <div>
     <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
     <home-swoper :banners="banners"></home-swoper>
+    <home-recommend :recommends="recommends"></home-recommend>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
   import HomeSwoper from './childComps/HomeSwiper'
+  import HomeRecommend from './childComps/HomeRecommend'
 
   import {getHomeMultidata} from 'network/home'
 
   export default {
     components:{
       NavBar,
-      HomeSwoper
+      HomeSwoper,
+      HomeRecommend
     },
     data() {
       return {
