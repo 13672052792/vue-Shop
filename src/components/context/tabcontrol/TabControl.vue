5a1fdb60bffa9d8e35c7418c77562617546e2bcc
wangyuan <themomentyuan@qq.com>
2020-07-15 16:02:15 +0800

    'featureView与tabControl模块添加'

---
+++ src/components/context/tabcontrol/TabControl.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in control" 
+         :key="index" 
+         class="tab-control-item"
+         :class="{active: index === controlIndex}"
+         @click="itemClick(index)"
+         >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    control: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      controlIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.controlIndex = index;
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    font-size: 15px;
+    background-color: #fff;
+  }
+
+  .tab-control-item{
+    flex: 1;
+  }
+
+  .tab-control-item span{
+    padding: 5px;
+  }
+
+  .active{
+    color: var(--color-high-text);
+    
+  }
+
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,94 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
     <home-swoper :banners="banners"></home-swoper>
     <home-recommend :recommends="recommends"></home-recommend>
+    <feature-view></feature-view>
+    <tab-control :control="['流行','新款','精选']" class="control-style"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li><li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/context/tabcontrol/TabControl'
+
   import HomeSwoper from './childComps/HomeSwiper'
   import HomeRecommend from './childComps/HomeRecommend'
+  import FeatureView from './childComps/FeatureView'
 
   import {getHomeMultidata} from 'network/home'
 
   export default {
     components:{
       NavBar,
+      TabControl,
       HomeSwoper,
-      HomeRecommend
+      HomeRecommend,
+      FeatureView
     },
     data() {
       return {
@@ -40,8 +111,25 @@
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
+
 .nav-bar{
   background-color: var(--color-high-text);
   color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 2;
 }
+
+.control-style{
+  position: sticky;
+  top: 44px;
+}
+
+
 </style>
